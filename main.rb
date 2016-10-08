15b0250fd41484f11117965e1725fbef6b39e416
57160034 <57160034@go.buu.ac.th>
2016-10-08 14:36:37 +0700

    Update and rename .gitignore to main.rb

---
+++ main.rb
@@ -0,0 +1,22 @@
+puts "ข้อ 1==="
+n = 10
+if n<1
+   puts "Number Worng!"
+else
+    nub = 1
+    a = ([1])
+   until nub > n
+    a << nub ** 3
+    #puts nub
+    nub = nub + 1
+end
+     puts a
+end
+  
+  
+puts "ข้อ 2"
+
+def prime_fib (number)
+    puts "Hello " + msg
+end
+
